a1a89889449ba2be812f4195cd243335629f6c79
MariusDinu <doskyas@gmail.com>
2020-03-15 15:49:04 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    public GameObject pro;
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+ 
+    void Update()
+    {
+        MouseMovement();
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pro, transform.position, Quaternion.identity);
+
+            Projectile project = pro.GetComponent<Projectile>();
+            project.movementDirection = Vector2.right;
+        }
+    }
+
+    void MouseMovement()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        this.transform.position = new Vector3(worldPosition.x, worldPosition.y, this.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Vector3 movementDirection;
+    public float speed;
+    public float lifetime = 2.0f;
+    void Start()
+    {
+        
+    }    
+    void Update()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = movementDirection * speed;
+        //  1.0 -> 50.0
+        DestroyObject();
+    }
+
+    void DestroyObject()
+    {
+        lifetime -= Time.deltaTime; // count down timer + to increase
+
+        if (lifetime <= 0)
+            Destroy(this.gameObject);
+    }
+}
